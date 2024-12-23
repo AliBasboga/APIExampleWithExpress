281ea1a738891759ac59c61e5ca0af0cb69cddfe
sanjeev <kumarsanjeevdutta02@gmail.com>
2024-12-23 13:33:45 +0530

    fixed the rate limit issue

--- api/country-api/index.js
+++ api/country-api/index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const cors = require('cors'); // CORS desteği eklemek için
 const app = express();
 
+// importing rate limit package and setting attributes
+const rateLimit = require('express-rate-limit')
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes (change as your requirement)
+	limit: 30, // Limit each IP to 30 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    message: "Too many requests, please try after 15 mins.",
+    statusCode: 429
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
+
 // CORS orta katmanını ekle
 app.use(cors());
 
